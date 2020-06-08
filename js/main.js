b97d604d683b15fa750eaf5e07afeccdedade07f
AlexShelter <allinshelter@gmail.com>
2020-06-08 19:49:06 +0400

    add lesson02

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-let money;
-let income;
-let addExpenses;
-let deposit;
-let mission;
-let period;
+let money = 300000;
+console.log(typeof money);
 
-alert('hello');
+let income = 'freelance';
+console.log(typeof income);
 
-console.log('world');
+let addExpenses = 'Internet, Taxi, Phone';
+console.log(addExpenses.length);
+
+let deposit = false;
+console.log(typeof deposit);
+
+let mission = 90000000;
+let period = 1;
+
+console.log('Период равен ' + period + ' месяцев. ' + 'Цель заработать ' + mission +
+ ' долларов.');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+
+let budgetDay = money/30;
+console.log(budgetDay);
